441452140726b3fd53f4eb38bf24489ca62e372c
Vladimir Müller <code@vladmueller.de>
2025-02-11 16:25:57 +0100

    🔧 Also sync resource from Obsidian

--- sync_posts.bat
+++ sync_posts.bat
@@ -4,3 +4,5 @@
 for /f "tokens=1,2 delims==" %%a in (.env) do set %%a=%%b
 
 robocopy "%OBSIDIAN_POSTS%" "%HUGO_POSTS%" /E "*.md"
+
+robocopy "%OBSIDIAN_POSTS_RESOURCES%" "%HUGO_POSTS_RESOURCES%" /E
