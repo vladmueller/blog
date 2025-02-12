621789197be371fd8b768af96873043625fca6e5
Vladimir Müller <code@vladmueller.de>
2025-02-12 18:49:26 +0100

    🚧 Load variables from environment file

--- python/sync.py
+++ python/sync.py
@@ -1,1 +1,17 @@
-print("Hello World")
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve the variables
+OBSIDIAN_POSTS = os.getenv("OBSIDIAN_POSTS")
+OBSIDIAN_POSTS_RESOURCES = os.getenv("OBSIDIAN_POSTS_RESOURCES")
+HUGO_POSTS = os.getenv("HUGO_POSTS")
+HUGO_POSTS_RESOURCES = os.getenv("HUGO_POSTS_RESOURCES")
+
+# Print the variables to verify they are loaded
+print("OBSIDIAN_POSTS:", OBSIDIAN_POSTS)
+print("OBSIDIAN_POSTS_RESOURCES:", OBSIDIAN_POSTS_RESOURCES)
+print("HUGO_POSTS:", HUGO_POSTS)
+print("HUGO_POSTS_RESOURCES:", HUGO_POSTS_RESOURCES)
