0380f923b7d26d530e4ad91e2802603c85af6ea9
Vladimir Müller <code@vladmueller.de>
2025-02-12 09:12:27 +0100

    🔧 Do not copy subdirectories

--- sync_posts.bat
+++ sync_posts.bat
@@ -3,6 +3,6 @@
 :: Load .env
 for /f "tokens=1,2 delims==" %%a in (.env) do set %%a=%%b
 
-robocopy "%OBSIDIAN_POSTS%" "%HUGO_POSTS%" /E "*.md"
+robocopy "%OBSIDIAN_POSTS%" "%HUGO_POSTS%" "*.md" /LEV:1
 
 robocopy "%OBSIDIAN_POSTS_RESOURCES%" "%HUGO_POSTS_RESOURCES%" /E
