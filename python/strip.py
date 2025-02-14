6ac1c8fb2658bde0464630ce6d34c29b4bf3db8c
Vladimir Müller <code@vladmueller.de>
2025-02-14 15:52:37 +0100

    🐍 Remove prefixed dates in my Markdown links

--- python/strip.py
+++ python/strip.py
@@ -13,11 +13,15 @@ if not os.path.isdir(HUGO_POSTS):
 
 md_files = glob.glob(os.path.join(HUGO_POSTS, "*.md"))
 
+date_pattern = re.compile(r"\[(\d{4}-\d{2}-\d{2}) (.*?)\]\((.*?)\)")
+
 for md_file in md_files:
     with open(md_file, "r", encoding="utf-8") as f:
         content = f.read()
 
     new_content = re.split(r"^## Hidden\s*---", content, flags=re.MULTILINE)[0].strip()
+
+    new_content = date_pattern.sub(r"[\2](\3)", new_content)
 
     with open(md_file, "w", encoding="utf-8") as f:
         f.write(new_content + "\n")
